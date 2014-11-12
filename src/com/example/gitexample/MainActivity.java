108b956b876db28d8c4fbc9624e5240bcd397930
Administrator <Administrator@catherinezeng>
2014-11-12 18:34:45 +0800

    finish

--- src/com/example/gitexample/MainActivity.java
+++ src/com/example/gitexample/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		System.out.println("AAAAAAAAAAA");
+		System.out.println("Finish V1");
 
 	}
 
