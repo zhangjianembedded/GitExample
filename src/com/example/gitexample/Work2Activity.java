bf45d44a5cc4a913d4a3a32132597336f8b23dc7
zhangjian <zhangjianembedded@yea.net>
2014-11-13 11:46:57 +0800

    modify Work2Activity

--- src/com/example/gitexample/Work2Activity.java
+++ src/com/example/gitexample/Work2Activity.java
@@ -1,5 +1,16 @@
 package com.example.gitexample;
 
-public class Work2Activity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class Work2Activity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
 }
